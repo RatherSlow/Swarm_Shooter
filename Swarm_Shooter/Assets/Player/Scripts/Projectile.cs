3a3f955f3167fc3b5aadbf203d8b79c2d36680f3
Callum Casey <123276837+RatherSlow@users.noreply.github.com>
2023-11-23 09:39:15 +0000

    Update Projectile.cs
    Projectiles will be destroyed after a period of time (not assigned yet)

--- Swarm_Shooter/Assets/Player/Scripts/Projectile.cs
+++ Swarm_Shooter/Assets/Player/Scripts/Projectile.cs
@@ -7,5 +7,33 @@ public class Projectile : MonoBehaviour
     public Rigidbody rb;
     public LayerMask whatIsBees;
 
-    public float maxLifetime;
+    [SerializeField]
+    float tick;
+    [SerializeField]
+    float maxLifetime;
+    bool LifetimeEnd;
+
+    void Update()
+    {
+        Destruction();
+    }
+
+
+    public bool Lifetime()
+    {
+    if (tick < maxLifetime)
+        {
+            tick += Time.deltaTime;
+            return false;
+        }
+        return true;
+    }
+    public void Destruction()
+    {
+        LifetimeEnd = Lifetime();
+        if(LifetimeEnd)
+        {
+            Destroy(gameObject);
+        }
+    }    
 }
